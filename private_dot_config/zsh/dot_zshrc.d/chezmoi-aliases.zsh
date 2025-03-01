6ff5536d61e3fd6d6b2de732a867228126402270
Amaury Constant <amoconst@gmail.com>
2025-03-01 17:08:24 +0100

    Add short aliases for `chezmoi`

---
+++ private_dot_config/zsh/dot_zshrc.d/chezmoi-aliases.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+#
+# chezmoi-aliases.zsh - Aliases for chezmoi
+#
+
+alias cminit='chezmoi init --force'
+alias cmapply='chezmoi apply -v'
+alias cmedit='chezmoi edit'
+alias cmadd='chezmoi add'
