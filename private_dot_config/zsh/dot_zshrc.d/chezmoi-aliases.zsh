6a512a304b51fa9a1dac1497a9a9f32e044cbe9c
Amaury Constant <amoconst@gmail.com>
2025-03-05 11:17:44 +0100

    Add aliases for `chezmoi_modify_manager`

--- private_dot_config/zsh/dot_zshrc.d/chezmoi-aliases.zsh
+++ private_dot_config/zsh/dot_zshrc.d/chezmoi-aliases.zsh
@@ -7,3 +7,6 @@ alias cminit='chezmoi init --force'
 alias cmapply='chezmoi apply -v'
 alias cmedit='chezmoi edit'
 alias cmadd='chezmoi add'
+
+alias cmmmadd='chezmoi_modify_manager -a'
+alias cmmmadd-tmpl='chezmoi_modify_manager -s -t=path-tmpl'
