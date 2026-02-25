05ded848156d4c1430f34620d2613993a289aedd
Amaury Constant <amoconst@gmail.com>
2026-02-25 09:54:37 +0100

    Reorganize zsh aliases and add lazydocker/opencode shortcuts

--- private_dot_config/zsh/dot_zshrc.d/aliases.zsh
+++ private_dot_config/zsh/dot_zshrc.d/aliases.zsh
@@ -3,19 +3,15 @@
 # aliases.zsh - Set whatever shell aliases you want.
 #
 
+# Modern drop-in replacemente
 alias http=xh
-
-alias fdf='fd --type file'
-alias fdd='fd --type directory'
-
 alias ls='eza --group-directories-first'
 alias ll='eza -la --group-directories-first'
 alias la='eza -a'
 alias tree='eza --tree'
-
 alias jq='jaq'
-
 alias cd='z'
+alias du='dust'
 
 # Tar
 alias tarls="tar -tvf"
@@ -25,16 +21,15 @@ alias untar="tar -xf"
 alias p=pnpm
 
 # AI
-alias claude='clear && claude'
-
-# Disk usage
-alias du='dust'
+alias cc='clear && claude'
+alias opc='opencode'
 
 # Shortcuts
 alias lgit='lazygit'
+alias ldck='lazydocker'
 
 # Symlink
 alias lns='ln -sfn'
 
-# Symlink
+# Twiggit
 alias twg='twiggit'
